f08479bad0f0be6f525b479b8f3de73cb2d5d353
NDark Teng <ndarkteng@gmail.com>
2013-06-01 14:45:37 +0800

    modify prefab

---
+++ COURSE/CameraDemonstration01/Assets/Editor/FBXScaleFix.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+ 
+public class FBXScaleFix : AssetPostprocessor
+{
+	public void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = (ModelImporter) assetImporter;
+		modelImporter.globalScale = 1;
+	}	
+}
