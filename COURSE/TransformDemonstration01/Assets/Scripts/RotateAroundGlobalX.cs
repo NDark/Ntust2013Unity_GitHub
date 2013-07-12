b2ced61e03fef4715ad42e2727b2e27b3a5cb2b3
NDarkTeng <ndarkteng@gmail.com>
2013-07-12 21:42:11 +0800

    implement TransformDemonstration01/Assets/Scenes/TransformDemonstration01.unity

---
+++ COURSE/TransformDemonstration01/Assets/Scripts/RotateAroundGlobalX.cs
@@ -0,0 +1,24 @@
+/*
+ * @file RotateAroundGlobalX.cs
+ * @author NDark
+ * @date 20130712 . file started.
+ */
+using UnityEngine;
+using System.Collections;
+
+public class RotateAroundGlobalX : MonoBehaviour 
+{
+	Vector3 m_Right = new Vector3( 1 , 0 , 0 ) ;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.Rotate( m_Right , 1 , Space.World ) ;
+	}
+}
+ 
