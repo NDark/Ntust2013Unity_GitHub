3791b400b1ae45a674defd2043b115f56ae8ac37
NDarkTeng <ndarkteng@gmail.com>
2013-07-12 20:19:43 +0800

    implement CrabRevolution02.unity

---
+++ COURSE/TransformDemonstration01/Assets/Scripts/LookAtBall02.cs
@@ -0,0 +1,45 @@
+/**
+ * @file LookAtBall02.cs
+ * @author NDark
+ * @date 20130712 . file started.
+ */
+using UnityEngine;
+using System.Collections;
+
+public class LookAtBall02 : MonoBehaviour 
+{
+	GameObject m_Ball = null ;
+	// Use this for initialization
+	void Start () 
+	{
+		if( null == m_Ball )
+		{
+			InitializeBall() ;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( null == m_Ball )
+			return ;
+		
+		Vector3 toBall = m_Ball.transform.position - this.gameObject.transform.position ;
+		
+		this.gameObject.transform.rotation = Quaternion.LookRotation( toBall ) ;
+	}
+	
+	private void InitializeBall()
+	{
+		m_Ball = GameObject.Find( "Ball" ) ;
+		if( null == m_Ball )
+		{
+			Debug.LogError( "LookAtBall02::InitializeBall() null == m_Ball" ) ;
+		}
+		else
+		{
+			Debug.Log( "LookAtBall02::InitializeBall() end." ) ;
+		}		
+	}
+}
