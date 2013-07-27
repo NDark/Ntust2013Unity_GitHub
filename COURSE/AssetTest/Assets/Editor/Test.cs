cbfe64720aafb49c2e47899ca6ae40982f5b1c32
NDarkAtR612 <ndarkteng@gmail.com>
2013-07-27 13:16:26 +0800

    implement project AssetTest

--- COURSE/AssetTest/Assets/Editor/Test.cs
+++ COURSE/AssetTest/Assets/Editor/Test.cs
@@ -6,9 +6,19 @@ public class ExportAssetBundles {
 	
 	
 	[MenuItem ("Build/BuildWebplayerStreamed")]
-	static void MyBuild()
+	static void BuildStreamedSceneAssetBundle()
 	{
 		string []levels = {"Assets/Scenes/Test.unity"} ;
-		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Streamed-Level1.unity3d", BuildTarget.StandaloneWindows ); 
+		BuildPipeline.BuildStreamedSceneAssetBundle( 
+			levels, "BuildStreamedSceneAssetBundle.unity3d", BuildTarget.StandaloneWindows ); 
 	}
+	
+	[MenuItem ("Build/BuildAsset")]
+	static void ExportSelected()
+	{
+		Debug.Log( "ExportSelected()" + Selection.activeObject.name ) ;
+		// Build the resource file from the active selection.
+		BuildPipeline.BuildAssetBundle(
+			Selection.activeObject, Selection.objects, "BuildAssetBundle.unity3d" ) ;
+	}	
 }
