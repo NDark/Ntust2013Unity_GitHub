74a1c82e3b81f791a30bab32d7a2f1c6c92bb67b
NDarkAtR612 <ndarkteng@gmail.com>
2013-08-10 12:21:17 +0800

    OnMouseSelection02.cs . add code of do not overlay 2 gui.

--- COURSE/GUITexture02/Assets/Scripts/OnMouseSelection02.cs
+++ COURSE/GUITexture02/Assets/Scripts/OnMouseSelection02.cs
@@ -2,6 +2,7 @@
 @file OnMouseSelection02.cs
 @author NDark
 @date 20130804 file started.
+@date 20130810 by NDark . add code of do not overlay 2 gui.
 */
 using UnityEngine;
 
@@ -17,7 +18,6 @@ public class OnMouseSelection02 : MonoBehaviour
 			GameObject singleton = GameObject.Find( "GlobalSingleton" ) ;
 			m_SelectSystem = singleton.GetComponent<SelectionSystem02>() ;
 		}
-	
 	}
 	
 	// Update is called once per frame
@@ -37,21 +37,27 @@ public class OnMouseSelection02 : MonoBehaviour
 		else
 		{
 			m_SelectSystem.Select( true , this.gameObject ) ;
+			
+			// force cursor to close
+			m_SelectSystem.ShowCursor( false ) ;			
 		}
 	}
-
 	
 	void OnMouseEnter()
 	{
 		if( null == m_SelectSystem )
 			return ;
-		m_SelectSystem.ShowCursor( true ) ;
+		
+		// do not overlay 2 gui
+		if( m_SelectSystem.m_SelectObject != this.gameObject )				
+			m_SelectSystem.ShowCursor( true ) ;
 	}
 
 	void OnMouseExit()
 	{
 		if( null == m_SelectSystem )
 			return ;
+		
 		m_SelectSystem.ShowCursor( false ) ;
 	}	
 }
