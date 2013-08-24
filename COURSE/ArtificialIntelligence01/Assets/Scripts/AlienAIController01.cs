99659582c32fa6f408fa0d00622aebfd38c93c64
NDarkR612 <ndarkteng@gmail.com>
2013-08-24 16:16:19 +0800

    format

--- COURSE/ArtificialIntelligence01/Assets/Scripts/AlienAIController01.cs
+++ COURSE/ArtificialIntelligence01/Assets/Scripts/AlienAIController01.cs
@@ -22,17 +22,20 @@ public class AlienAIController01 : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 mainCharacterForward = m_MainCharacter.transform.forward ;
-		Vector3 toMainCharacter = m_MainCharacter.transform.position - this.gameObject.transform.position ;
+		Vector3 mainCharacterForward = 
+			m_MainCharacter.transform.forward ;
 		
-		FollowTheMainCharacter02 script = this.gameObject.GetComponent<FollowTheMainCharacter02>() ;
+		Vector3 toMainCharacter = 
+			m_MainCharacter.transform.position - this.gameObject.transform.position ;
+		
+		AlienAIController01 script = this.gameObject.GetComponent<AlienAIController01>() ;
 		
 		float angle = Vector3.Angle( mainCharacterForward , toMainCharacter ) ;
 		if( angle < 90 )
 		{
 			if( null == script )
 			{
-				script = this.gameObject.AddComponent<FollowTheMainCharacter02>() ;
+				script = this.gameObject.AddComponent<AlienAIController01>() ;
 				script.m_MainCharacter = this.m_MainCharacter ;
 			}
 			script.enabled = true ;
@@ -50,11 +53,11 @@ public class AlienAIController01 : MonoBehaviour
 		
 		if( null == m_MainCharacter )
 		{
-			Debug.LogError( "FollowTheMainCharacter02:InitializeMainCharacterObjectPtr() null == m_MainCharacter" ) ;
+			Debug.LogError( "AlienAIController01:InitializeMainCharacterObjectPtr() null == m_MainCharacter" ) ;
 		}
 		else
 		{
-			Debug.Log( "FollowTheMainCharacter02:InitializeMainCharacterObjectPtr() end." ) ;
+			Debug.Log( "AlienAIController01:InitializeMainCharacterObjectPtr() end." ) ;
 		}
 	}
 }
