b635087bce40bb93c09cbfee9191bfe404053181
NDarkAtR612 <ndarkteng@gmail.com>
2013-09-14 14:34:09 +0800

    add project NavMesh01_Empty

---
+++ COURSE/NavMesh01_Empty/Assets/Scripts/FollowTargetByNavMesh01.cs
@@ -0,0 +1,34 @@
+/*
+@file FollowTargetByNavMesh01.cs
+@author NDark
+@date 20130809 by NDark
+*/
+using UnityEngine;
+
+public class FollowTargetByNavMesh01 : MonoBehaviour 
+{
+	
+	public GameObject m_TargetObj = null ;
+	public NavMeshAgent m_NavMeshAgent = null ;
+	// Use this for initialization
+	void Start () 
+	{
+		m_NavMeshAgent = this.gameObject.GetComponent<NavMeshAgent>() ;
+		if( null == m_NavMeshAgent )
+		{
+			Debug.LogError( "null == m_NavMeshAgent )" ) ;
+			return ;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( null == m_TargetObj || 
+			null == m_NavMeshAgent )
+			return ;
+		
+		m_NavMeshAgent.SetDestination( m_TargetObj.transform.position ) ;
+	
+	}
+}
