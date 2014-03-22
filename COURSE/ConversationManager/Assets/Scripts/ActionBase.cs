382f82c8b80d528747745c1474efb106d63aa9aa
NDark <ndarkteng@gmail.com>
2014-03-22 21:38:05 +0800

    add ActionBase.cs add ConditionBase.cs

---
+++ COURSE/ConversationManager/Assets/Scripts/ActionBase.cs
@@ -0,0 +1,14 @@
+﻿/**
+ * @file ActionBase.cs
+ * @author NDark
+ * @date 20140322 . file started.
+ */
+using UnityEngine;
+
+public class ActionBase 
+{
+	public virtual void DoAction()
+	{
+		
+	}
+}
