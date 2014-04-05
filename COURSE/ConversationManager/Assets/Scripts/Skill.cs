07caa17418e99745025fb23c215ac359ebb31e32
NDark <ndarkteng@gmail.com>
2014-04-05 20:10:51 +0800

    add FightSystem scene

---
+++ COURSE/ConversationManager/Assets/Scripts/Skill.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * @file Skill.cs
+ * @author NDark
+ * @date 20140405 file started.
+ */
+using UnityEngine;
+
+public class Skill  
+{
+	public string DisplayString
+	{
+		get { return m_DisplayString ; }
+		set { m_DisplayString = value ; }
+	}
+	private string m_DisplayString = "" ;
+}
---
+++ COURSE/ConversationManager/Assets/Scripts/SkillAnimation.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * @file SkillAnimation.cs
+ * @author NDark
+ * @date 20140405 file started.
+ */
+using UnityEngine;
+using System.Collections;
+
+public class SkillAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
