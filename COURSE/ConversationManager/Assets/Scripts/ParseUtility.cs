ef05194f03e32f9c8eb01887062e6dba4060843f
NDark <ndarkteng@gmail.com>
2014-04-06 12:12:11 +0800

    modify xml parse

--- COURSE/ConversationManager/Assets/Scripts/ParseUtility.cs
+++ COURSE/ConversationManager/Assets/Scripts/ParseUtility.cs
@@ -12,6 +12,25 @@ using System.Xml ;
 
 public static class ParseUtility  
 {
+	public static bool ParseMapStaticObject( 
+	                                      #if USE_XML
+	                                      XmlNode _node ,
+	                                      #endif // USE_XML
+	                                      
+	                                      ref string _unitName ,
+	                                      ref string _prefabTemplateName , 
+	                                      ref Vector3 _position , 
+	                                      ref Quaternion _orientation )
+	{
+		#if USE_XML
+		bool ret = XMLParseLevelUtility.CheckAndParseStaticObject( _node , 
+		                                                  ref _unitName ,
+		                                                  ref _prefabTemplateName , 
+		                                                  ref _position , 
+		                                                  ref _orientation ) ;
+		return ret ;
+		#endif // USE_XML
+	}
 
 	public static bool ParseMapZoneObject( 
 #if USE_XML
@@ -21,17 +40,17 @@ public static class ParseUtility
 	                               ref string _unitName ,
 	                              ref string _prefabTemplateName , 
 	                              ref Vector3 _position , 
-	                              ref Quaternion _orientation , ref string _TextureName )
+	                              ref Quaternion _orientation , 
+	                                      ref string _TextureName )
 	{
 		#if USE_XML
-		bool ret = XMLParseLevelUtility.ParseStaticObject( _node , 
+		bool ret = XMLParseLevelUtility.CheckAndParseMapZoneObject( _node , 
 		                                                   ref _unitName ,
-		                                                   ref _prefabTemplateName , 
-		                                                   ref _position , 
-		                                                   ref _orientation ) ;
+		                                                           ref _prefabTemplateName , 
+		                                                           ref _position , 
+		                                                           ref _orientation ,
+		                                                           ref _TextureName ) ;
 
-		if( null != _node.Attributes[ "TextureName" ] )
-			_TextureName = _node.Attributes[ "TextureName" ].Value ;
 
 		return ret ;
 		#endif // USE_XML
