d62a13d1f3135e6381a0424634e83bb330e88188
NDark <ndarkteng@gmail.com>
2014-04-20 22:07:00 +0800

    將 UnitDataStruct.cs 與 UnitDataSetting 的 standardParam 拆開來

--- COURSE/ConversationManager/Assets/Scripts/UnitDataStruct.cs
+++ COURSE/ConversationManager/Assets/Scripts/UnitDataStruct.cs
@@ -21,10 +21,28 @@ public class UnitDataStruct
 		return this.m_UnitDataSetting.GetUnitType() ;
 	}
 
+
+	// 這個是 UnitDataStruct 本身的 StandardParameter (變數), 而不是 m_UnitDataSetting 內的資料
+	// 注意
+	public Dictionary< string , StandardParameter > standardParameters = new Dictionary< string , StandardParameter >() ;
+	public StandardParameter [] Debug_standardParameters = null ;
+	
 	public void ImportStandardParameter( Dictionary< string , StandardParameter > _ParameterMap )
 	{
-		this.m_UnitDataSetting.ImportStandardParameter( _ParameterMap ) ;
+		Dictionary< string , StandardParameter >.Enumerator i = _ParameterMap.GetEnumerator() ;
+		while( i.MoveNext() )
+		{
+			AssignStandardParameter( i.Current.Key , i.Current.Value ) ;
+		}
 	}
+	
+	public void AssignStandardParameter( string _Key , 
+	                                    StandardParameter _Parameter )
+	{
+		standardParameters[ _Key ] = new StandardParameter( _Parameter ) ;
+	}
+
+
 
 	public void ImportSkills( Dictionary< string , Skill > _Skills )
 	{
@@ -50,7 +68,7 @@ public class UnitDataStruct
 
 	public Dictionary< string , StandardParameter > GetStandardParameterTable()
 	{
-		return m_UnitDataSetting.standardParameters ;
+		return standardParameters ;
 	}
 
 	public Dictionary< string , Skill > GetSkillTable()
@@ -58,9 +76,16 @@ public class UnitDataStruct
 		return m_UnitDataSetting.m_Skills ;
 	}
 
+	public void Import( UnitDataSetting _SettingSrc ) 
+	{
+		m_UnitDataSetting = _SettingSrc ;// 共用
+		ImportStandardParameter( _SettingSrc.standardParameters ) ;// 自己複製一份
+	}
+
 	public void Import( UnitDataStruct _Src ) 
 	{
-		m_UnitDataSetting.Import( _Src.m_UnitDataSetting ) ;
+		m_UnitDataSetting = _Src.m_UnitDataSetting ;// 共用
+		ImportStandardParameter( _Src.m_UnitDataSetting.standardParameters ) ;// 自己複製一份
 		m_UnitState = _Src.m_UnitState ;
 		
 	}
