a715e9883ff02bd135fed785907cda0b5140ed0f
NDark <ndarkteng@gmail.com>
2014-05-04 13:43:38 +0800

    實作招式的屬性資料

--- COURSE/ConversationManager/Assets/Scripts/Skill.cs
+++ COURSE/ConversationManager/Assets/Scripts/Skill.cs
@@ -9,6 +9,56 @@ using UnityEngine;
 
 public class Skill
 {
+	public string Label
+	{
+		get { return m_Label ; }
+		set { m_Label = value ; }
+	}
+	private string m_Label = "" ;
+	
+	public string Type
+	{
+		get { return m_Type ; }
+		set { m_Type = value ; }
+	}
+	private string m_Type = "" ;	
+	
+	
+	public string Target
+	{
+		get { return m_Target ; }
+		set { m_Target = value ; }
+	}
+	private string m_Target = "" ;
+	
+	public string DefenseProperty
+	{
+		get { return m_DefenseProperty ; }
+		set { m_DefenseProperty = value ; }
+	}
+	private string m_DefenseProperty = "" ;	
+	
+	public string AttactProperty
+	{
+		get { return m_AttactProperty ; }
+		set { m_AttactProperty = value ; }
+	}
+	private string m_AttactProperty = "" ;	
+		
+	public string FyingSpeed
+	{
+		get { return m_FyingSpeed ; }
+		set { m_FyingSpeed = value ; }
+	}
+	private string m_FyingSpeed = "" ;	
+
+	public string AnimationPrefab
+	{
+		get { return m_AnimationPrefab ; }
+		set { m_AnimationPrefab = value ; }
+	}
+	private string m_AnimationPrefab = "" ;	
+	
 	public string DisplayString
 	{
 		get { return m_DisplayString ; }
