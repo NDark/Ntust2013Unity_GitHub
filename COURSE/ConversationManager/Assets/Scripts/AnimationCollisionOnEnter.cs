c819ba2ab1357c69293fb4d3384efc9bcc65c679
NDark <ndarkteng@gmail.com>
2014-05-04 13:47:44 +0800

    . 修改移除 target player 的設定

--- COURSE/ConversationManager/Assets/Scripts/AnimationCollisionOnEnter.cs
+++ COURSE/ConversationManager/Assets/Scripts/AnimationCollisionOnEnter.cs
@@ -1,15 +1,15 @@
 ﻿/**
- * @file AnimationCollisionOnEnter.cs
- * @author NDark
- * @date 20140406 file started.
- * @date 20140412 by NDark . 修改 m_TargetPlayer 與 other.name 不相同的錯誤.
+@file AnimationCollisionOnEnter.cs
+@author NDark
+@date 20140406 file started.
+@date 20140412 by NDark . 修改 m_TargetPlayer 與 other.name 不相同的錯誤.
+@date 20140504 by NDark . 修改移除 target player 的設定
  */
 using UnityEngine;
 using System.Collections.Generic;
 
 public class AnimationCollisionOnEnter : MonoBehaviour 
 {
-	public GameObject m_TargetPlayer = null ;
 	public string m_ParentName = "" ;
 
 	// Use this for initialization
@@ -25,28 +25,24 @@ public class AnimationCollisionOnEnter : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D other) 
 	{
-		// Debug.Log( "other" + other.name  ) ;
+		Debug.Log( "OnTriggerEnter2D() , other=" + other.name  ) ;
 		if( other.name != m_ParentName )
 		{
-			if( null != m_TargetPlayer /* && 
-			   other.name == m_TargetPlayer.name*/ )
+			UnitData unitData = other.GetComponent<UnitData>() ;
+			if( null != unitData )
 			{
-				UnitData unitData = m_TargetPlayer.GetComponent<UnitData>() ;
-				if( null != unitData )
+				Dictionary< string , StandardParameter > standardParamTable = unitData.m_UnitDataStruct.GetStandardParameterTable() ;
+				if( true == standardParamTable.ContainsKey( "HP" ) )
 				{
-					Dictionary< string , StandardParameter > standardParamTable = unitData.m_UnitDataStruct.GetStandardParameterTable() ;
-					if( true == standardParamTable.ContainsKey( "HP" ) )
-					{
-						float hpNow = standardParamTable[ "HP" ].now ;
-						--hpNow ;
-						standardParamTable[ "HP" ].now = hpNow ;
+					float hpNow = standardParamTable[ "HP" ].now ;
+					--hpNow ;
+					standardParamTable[ "HP" ].now = hpNow ;
 
-						FightSystem fs = GlobalSingleton.GetFightSystem() ;
-						string str = string.Format( "{0} 對 {1} 造成了 1 點傷害" , m_ParentName , m_TargetPlayer.name ) ;
-						fs.AddStatus( str ) ;
-						str = string.Format( "{0} 剩下 {1} 點生命值" , m_TargetPlayer.name , hpNow ) ;
-						fs.AddStatus( str ) ;
-					}
+					FightSystem fs = GlobalSingleton.GetFightSystem() ;
+					string str = string.Format( "{0} 對 {1} 造成了 1 點傷害" , m_ParentName , other.name ) ;
+					fs.AddStatus( str ) ;
+					str = string.Format( "{0} 剩下 {1} 點生命值" , other.name , hpNow ) ;
+					fs.AddStatus( str ) ;
 				}
 			}
 
