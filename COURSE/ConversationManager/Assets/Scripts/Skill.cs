d1899f383bc60d4e3a67afdcf5d253e94662e1ac
NDark <ndarkteng@gmail.com>
2014-05-10 17:18:47 +0800

    implement EnegyProperty

--- COURSE/ConversationManager/Assets/Scripts/Skill.cs
+++ COURSE/ConversationManager/Assets/Scripts/Skill.cs
@@ -19,35 +19,94 @@ public class EnegyProperty
 	
 	public void Parse( string _Str )
 	{
-	
+		string [] spliter1 = { "," } ;
+		string [] propertyVec = _Str.Split( spliter1 , System.StringSplitOptions.None ) ;
+		
+		string [] spliter2 = { ":" } ;
+		foreach( string propertyStr in propertyVec ) 
+		{
+			string [] propertyValue = propertyStr.Split( spliter2 , System.StringSplitOptions.None ) ;
+			if( propertyValue.Length >= 2 )
+			{
+				// propertyValue[ 0 ] ;// label
+				int value = 0 ;
+				int.TryParse( propertyValue[ 1 ] , out value ) ;
+				AddProperty( propertyValue[ 0 ] , value ) ;
+			}
+		}
 	}
 
 	public void Assign( EnegyProperty _Src ) 
 	{
-		
+		Clear() ;
+		Import( _Src ) ;
 	}
 	
 	public void Clear() 
 	{
-		
+		m_PropertyTable.Clear() ;
 	}
 		
 	public void Import( EnegyProperty _Src ) 
 	{
-	
+		Dictionary<string,int>.Enumerator i = _Src.m_PropertyTable.GetEnumerator() ;
+		while( i.MoveNext() )
+		{
+			AssignProperty( i.Current.Key , i.Current.Value ) ;
+		}
 	}
 	
 	public int GetProperty( string _Key ) 
 	{
 		int ret = 0 ;
+		if( true == m_PropertyTable.ContainsKey( _Key ) )
+		{
+			ret = m_PropertyTable[ _Key ] ;
+		}		
 		return ret ;
 	}
 	
 	public int AddProperty( string _Key , int _Value ) 
 	{
-		int result = 0 ;
+		int result = GetProperty( _Key ) + _Value ;
+		AssignProperty( _Key , result ) ;
 		return result ;	
+	}		
+	
+	public int AssignProperty( string _Key , int _Value ) 
+	{
+		int result = 0 ;
+		if( false == m_PropertyTable.ContainsKey( _Key ) )
+		{
+			m_PropertyTable.Add( _Key , _Value ) ;
+		}
+		else
+		{
+			m_PropertyTable[ _Key ] = _Value ;
+		}
+		
+		return result ;
 	}	
+	
+	public string CreatePropertyString()
+	{
+		string property = "" ;
+		string ret = "" ;
+		Dictionary<string,int>.Enumerator i = _Src.m_PropertyTable.GetEnumerator() ;
+		while( i.MoveNext() )
+		{
+			property = string.Format( "{0}:{1}" , i.Current.Key , i.Current.Value ) ;
+			if( 0 == ret.Length )
+			{
+				ret = property ;
+			}
+			else
+			{
+				ret = ret + "," + property ;
+			}
+		}
+		return ret ;
+	}
 		
 	private Dictionary<string,int> m_PropertyTable = new Dictionary<string, int>() ;
 }
