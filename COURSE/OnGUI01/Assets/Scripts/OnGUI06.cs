b17b1c6b9467a1b6d60d214be38213537d94b7a3
NDarkTeng <ndarkteng@gmail.com>
2015-05-31 14:46:16 +0800

    add OnGUI06.unity

---
+++ COURSE/OnGUI01/Assets/Scripts/OnGUI06.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnGUI06 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public string m_TextString = "(Click Button Please)" ;
+	public int m_Iteration = 0 ;
+	void OnGUI()
+	{
+		Func() ;
+	}
+
+	private void Func() 
+	{
+		Rect button1Rect = new Rect( 100, 100 , 300 , 50 ) ;
+		if( true == GUI.Button( button1Rect , m_TextString ) )
+		{ 
+			m_Iteration += 1 ;
+			m_TextString = "Oh yeah!!" ;
+		}
+		
+		Rect text2Rect = new Rect( 250, 250 , 100 , 50 ) ;
+		GUI.Label( text2Rect , m_Iteration.ToString() ) ;
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
