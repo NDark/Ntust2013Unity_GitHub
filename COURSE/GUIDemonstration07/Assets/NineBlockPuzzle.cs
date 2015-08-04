72f2881e67dda7986f4e978aee740e03e38ba28e
NDarkTeng <ndarkteng@gmail.com>
2015-08-04 23:03:24 +0800

    add NineBlockPuzzle.cs

---
+++ COURSE/GUIDemonstration07/Assets/NineBlockPuzzle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NineBlockPuzzle : MonoBehaviour 
+{
+	public GameObject[] m_Positions = null ;
+	public GameObject[] m_Objects = null ;
+	int currentIndex = 0 ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( Input.GetMouseButtonDown( 0 ) )
+		{
+			Vector3 pos = m_Positions[ currentIndex ].transform.position ;
+			pos.z = m_Objects[ currentIndex ].transform.position.z ;
+			m_Objects[ currentIndex ].transform.position = pos ;
+
+			currentIndex++ ;
+		}
+	}
+}
+
+
+
