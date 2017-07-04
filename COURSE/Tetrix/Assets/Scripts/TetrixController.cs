98b1e6b8f6dde42f3ec44983cc65913816fc5e90
ndarkpc <ndarkteng@gmail.com>
2017-07-04 19:55:18 +0800

    add UpdateBlockDownward()

--- COURSE/Tetrix/Assets/Scripts/TetrixController.cs
+++ COURSE/Tetrix/Assets/Scripts/TetrixController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TetrixController : MonoBehaviour {
 
+public class TetrixController : MonoBehaviour 
+{
+	public GameObject m_Block = null ;
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if( Time.time < m_NextCount )
+		{
+			return ;
+		}
+		
+		m_NextCount = Time.time + m_CountInterval ;
+		
+		UpdateBlockDownward() ;
 		
 	}
+	
+	void UpdateBlockDownward()
+	{
+		Vector3 pos = m_Block.transform.position ;
+		pos.y -= 1 ;
+		m_Block.transform.position = pos ;
+	}
+	
+	float m_NextCount = 0.0f ;
+	float m_CountInterval = 1.0f ;
 }
